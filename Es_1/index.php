da6ce4dc33fd9df9d9c25248d5d2fb60d889bc03
Alessio_Grieco <Alessiogrieco218@gmail.com>
2024-11-27 17:26:03 +0100

    Es_2 Completato

---
+++ Es_1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$Integer = 1;
+$Float = 1.6;
+$String = 'Alessio';
+$Boolean = true;
+
+echo $Integer;
+// echo $Float;
+// echo $String;
+// echo $Boolean;
+
+?>
---
+++ Es_2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$text1 = "Marco"; //Correggo "$1text" con "$text1"
+$text2 = "hai";  
+$text3 = "sete"; //Correggo "text.3" con "text3"
+$text4 = "?";
+$text5 = "Perchè"; //Correggo "@text5" con un prefisso "$"
+$text6 = "$text2"; //Correggo "$te-xt6" con "$text6" per una coesione di sintassi
+$text7 = 'bevuto'; //Correggo "$text 7" con "$text6" per una coesione di sintassi
+$text8 = "tutto."; //Correggo "$text8" con "$text8;" ed il suo dato con "tutto."
+
+echo "$text1 $text2 $text3 $text4 $text5 $text2 $text7 $text8";
+
+?>
